f19f5026d368f132d8f26f4a4302ee5d20a8c0ce
ahmedgamal1254 <ag1088768@gmail.com>
2025-05-02 18:23:32 +0200

    fix responsive design

--- src/components/layout/SidebarMobile.tsx
+++ src/components/layout/SidebarMobile.tsx
@@ -1,19 +1,8 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { FaBars, FaHamburger } from 'react-icons/fa';
-import Sidebar from './Sidebar';
+import { FaBars } from 'react-icons/fa';
 import SidebarMob from './SidebarMob';
-
 
 export default function SiderBarMobile() {
   const [siderbar, setSidebar] = React.useState<boolean>(false);
